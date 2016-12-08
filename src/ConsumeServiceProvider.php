dfc5364d0f01835d30cd426ea7e630c9e27ebc98
Tee <admin@jquerytips.com>
2016-12-08 18:40:31 +0700

    consume api package

---
+++ src/ConsumeServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Teepluss\Consume;
+
+use Illuminate\Support\ServiceProvider;
+
+class ConsumeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
